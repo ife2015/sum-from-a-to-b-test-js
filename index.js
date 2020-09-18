b0be1d042baebe7ce49fdf67b08baf6eb51c45ab
Ife Olaifa <ifeolaifa5@gmail.com>
2020-09-18 23:40:58 +0000

    Index Sum Recurrsion

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  //base case -> toN === fromN
+  // recursive case
+  // toN + sum(toN - 1)
+  if(fromN === toN) {
+    return fromN;
+  }
+  if(toN > fromN) {
+    return toN + sum(fromN, toN-1);
+  }
+
 }
 
 module.exports = sum;
